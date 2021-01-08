3691a975d897ac567cb5749335bb1ed3f7cd6d39
Kodirova <kodirovairoda7@gmail.com>
2021-01-08 17:00:00 +0500

    buttons of add, delete,update student

--- src/lms/windows/CreateStudentPanelWindow.java
+++ src/lms/windows/CreateStudentPanelWindow.java
@@ -5,25 +5,22 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import lms.Main;
 
-public class ListStudentPanelWindow {
+public class CreateStudentPanelWindow {
     @FXML
-      private void initialize() {
-    System.out.println("student list panel init");
-}
-
-
-      public static Scene getScene() throws Exception {
-        return Main.loadScene("list_student");
+    private void initialize() {
+        System.out.println("create student panel init");
     }
 
+
+    public static Scene getScene() throws Exception {
+        return Main.loadScene("create_student");
+    }
     @FXML
-    public void back(ActionEvent event) throws Exception {
+    public void confirm(ActionEvent actionEvent) throws Exception {
         Scene scene;
-        scene = AdminPanelWindow.getScene();
+        scene = ListStudentPanelWindow.getScene();
         Main.window.setScene(scene);
     }
-
-
     @FXML
     public void addstudent(ActionEvent actionEvent) throws Exception {
         Scene scene;
@@ -36,15 +33,9 @@ public class ListStudentPanelWindow {
         scene = ListStudentPanelWindow.getScene();
         Main.window.setScene(scene);
     }
-    @FXML
-    public void delete(ActionEvent actionEvent) throws Exception {
-        Scene scene;
-        scene = ListStudentPanelWindow.getScene();
-        Main.window.setScene(scene);
 
-    }
     @FXML
-    public void update(ActionEvent actionEvent) throws Exception {
+    public void back(ActionEvent event) throws Exception {
         Scene scene;
         scene = ListStudentPanelWindow.getScene();
         Main.window.setScene(scene);
