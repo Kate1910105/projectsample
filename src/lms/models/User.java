43c3f310859d4decdeb6c5d1422c5c769920539e
Author Name <kodirovairoda7@gmail.com>
2021-01-08 18:05:42 +0500

    some comments lol

--- src/lms/models/User.java
+++ src/lms/models/User.java
@@ -12,6 +12,7 @@ import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+// user class used to represent all roles
 public class User extends Model {
 
     public int id;
@@ -241,4 +242,5 @@ public class User extends Model {
             throw new AuthorizationError("Unknown");
         }
     }
+    //commmmmmments
 }
