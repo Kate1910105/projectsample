aa4810fc3ce3110b302f4660b7de0d632f12eaaf
Kodirova <kodirovairoda7@gmail.com>
2021-01-10 01:38:47 +0500

    List Book

--- src/lms/windows/ListBookPanelWindow.java
+++ src/lms/windows/ListBookPanelWindow.java
@@ -4,17 +4,47 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import lms.Main;
 import lms.models.Book;
 import lms.models.User;
+import lms.types.Role;
+
+import java.util.ArrayList;
 
 public class ListBookPanelWindow {
-    public TableView tableView;
+    public TableView<Book> tableView;
 
     @FXML
     private void initialize() {
         System.out.println("book list panel init");
+        ArrayList<Book> books = Book.all();
+        ArrayList<User> librarians = new ArrayList<>();
+
+
+
+        TableColumn<User, String> idColumn = new TableColumn<>("ID");
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tableView.getColumns().add(idColumn);
+
+        TableColumn<User, String> usernameColumn = new TableColumn<>("Username");
+        usernameColumn.setCellValueFactory(new PropertyValueFactory<>("username"));
+        tableView.getColumns().add(usernameColumn);
+
+        TableColumn<User, String> fullNameColumn = new TableColumn<>("Full Name");
+        fullNameColumn.setCellValueFactory(new PropertyValueFactory<>("fullName"));
+        tableView.getColumns().add(fullNameColumn);
+
+        TableColumn<User, String> roleColumn = new TableColumn<>("Role");
+        roleColumn.setCellValueFactory(new PropertyValueFactory<>("role"));
+        tableView.getColumns().add(roleColumn);
+
+        tableView.getItems().addAll(librarians);
+
+        System.out.println(books);
+
     }
 
 
