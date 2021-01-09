887b6bc20f4bafca68477a0cb40879b59dba6a06
CryPTON3264 <tashniyazovn@gmail.com>
2021-01-09 17:16:24 +0500

    Create Book

--- src/lms/windows/CreateBookPanelWindow.java
+++ src/lms/windows/CreateBookPanelWindow.java
@@ -3,9 +3,32 @@ package lms.windows;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import lms.Main;
+import lms.models.Book;
+import lms.models.User;
+import lms.types.BookStatus;
+import lms.types.Role;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class CreateBookPanelWindow {
+    public TextField title;
+    public TextField subject;
+    public TextField author;
+    public TextField isbn;
+    public DatePicker created;
+    public DatePicker publish;
+    public ComboBox status;
+    public String available;
+    public String borrowed;
+    public String reserved;
+
     @FXML
     private void initialize() {
         System.out.println("create book panel init");
@@ -34,5 +57,24 @@ public class CreateBookPanelWindow {
     }
 
     public void confirm(ActionEvent actionEvent) throws Exception {
+        Book book = new Book();
+        book.title = title.getText();
+        book.subject = subject.getText();
+        book.author = author.getText();
+        book.ISBN = isbn.getText();
+
+        LocalDate dateCreated = created.getValue();
+        book.createdAt = dateCreated.atStartOfDay();
+
+        LocalDate datePublish = publish.getValue();
+        book.publishDate = datePublish.atStartOfDay();
+
+
+        book.status = BookStatus.Reserved;
+        book.create();
+
+        Scene scene;
+        scene = ListBookPanelWindow.getScene();
+        Main.window.setScene(scene);
     }
 }
